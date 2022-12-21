2d847e6b5ad1845273995fce38974a71d25cc0a9
jnt0009 <31008913+jnt0009@users.noreply.github.com>
2022-12-20 23:18:21 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:latest
+
+LABEL Maintainer="JamariusTaylor"
+
+RUN mkdir /site
+
+COPY package.json /site
+
+COPY assets /site/assets
+
+COPY index.js /site
+
+COPY main.html /site
+
+WORKDIR /site
+
+EXPOSE 8080/tcp
+
+RUN npm install 
+
+CMD [ "node", "index.js" ]
+
+
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require('dotenv').config();
+// require("./pitCrew.js");
+
+const fs = require('fs');
+
+const app = express();
+
+const path = require('path');
+const router = express.Router();
+const port = process.env.PORT || 8080;
+
+// const view = __dirname + '/views'
+// const portfolio = __dirname + '/assets/portfolio'
+app.use(express.static('public'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+router.get('/',function(req,res){
+    console.log(router.stack);
+    // res.sendFile(path.join(__dirname +'/views/index.html'));
+    res.sendFile(__dirname + '/main.html')
+    //__dirname : It will resolve to your project folder.
+  });
+
+const startServer = async function() {
+    app.use('/', router);   
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+}
+
+startServer()
+  .then(x => console.log(x))
+  .catch(err => console.log(err))
